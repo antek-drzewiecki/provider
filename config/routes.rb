e49ac44c060acb1cfb7609e265deb606313b7930
Antek Drzewiecki <you.imagine@gmail.com>
2017-12-18 13:50:09 +0100

    Added a homepage with user login and logout links

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'home/index'
+
   devise_for :users
   use_doorkeeper
 
@@ -11,5 +13,7 @@ Rails.application.routes.draw do
 
     end
   end
+
+  root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
